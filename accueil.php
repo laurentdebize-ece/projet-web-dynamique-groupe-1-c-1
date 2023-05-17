1829331650faad21b418bac16b080ac99706785f
Brandolini Noe <noe.brandolini@edu.ece.fr>
2023-05-17 10:41:32 +0200

    a trier

---
+++ accueil.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Page d'accueil</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="accueil.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="image/logo_ece.png" width="180" height="50" alt="Logo"></a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+          
+        <li class="nav-item ml-2">
+          <a class="nav-link btn btn-outline-primary btn-case" href="etudiant.php"><span>&#8486Etudiant</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-outline-primary btn-case" href="#"><span>&#8508Professeur</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-outline-primary btn-case" href="administration."><span>&#x2605Administration</span></a>
+        </li>
+          
+        
+      </ul>
+    </div>
+  </nav>
+
+  <div class="welcome-section">
+  <img src="image/inseec-ece-fond.jpg" alt="Imagedefond">
+    <div class="filter"></div>
+    <div class="text-bar">
+      Bienvenue sur L'intranet de l'ECE Paris - Lyon
+    </div>
+
+
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
